8046b835fed44908485366985bc515dab634de6e
2swamped4u <jvrjsanchez@gmail.com>
2021-10-29 09:06:46 -0300

    Naive Bayes Tutorial

--- naive_bayes/example.py
+++ naive_bayes/example.py
@@ -1,5 +1,15 @@
+##Machine learning explores the study and construction of algorithms
+##that can learn from and make predictions on data.
+
+##Closely related to computational statistics
+
+##Used to devise complez models and algorithms that lend themselves 
+## to a prediction which in commericial use is known as predictive analysis.
 
 
-
+##Supervised Learning
 a = 4 + 2 % 5;
 print(a)
+
+
+##Unsupervised Learning
