881c0d03a03d9557ebcb3aab83f39e2d90a3be67
2swamped4u <jvrjsanchez@gmail.com>
2021-10-29 10:33:02 -0300

    Golf Play Days

--- naive_bayes/example.py
+++ naive_bayes/example.py
@@ -19,3 +19,38 @@ from  sklearn.naive_bayes import MultinomialNB
 df = pd.read_csv('golf_df.csv')
 
 print(df.head())
+
+
+print(df.info())
+
+df = df.apply(lambda x : x.astype('category'))
+
+df1 = df.apply(lambda x : x.cat.codes)
+
+#%%
+
+train = df[:10]
+test = df1[-4:]
+
+y_train = train.pop('Play')
+x_train = train
+
+y_test = test.pop('Play')
+x_test = test
+
+#%%
+
+model = MultinomialNB()
+
+model_obj = model.fit(x_test, y_test)
+#model_obj = model.fit(x_train, y_train) 
+
+#%%
+
+y_out = model.predict(x_test)
+#y_out = model.predict(x_train)
+
+#%%
+print('Testing Accuracy: ', model.score(x_test, y_test))
+#print('Training Accuracy: ', model.score(x_train, y_train))
+
