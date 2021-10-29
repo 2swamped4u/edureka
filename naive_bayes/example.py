942c792716523f1297fe04519479ac256ff13347
2swamped4u <jvrjsanchez@gmail.com>
2021-10-29 10:09:24 -0300

    Golf Play Days

--- naive_bayes/example.py
+++ naive_bayes/example.py
@@ -1,15 +1,21 @@
 ##Machine learning explores the study and construction of algorithms
 ##that can learn from and make predictions on data.
-
 ##Closely related to computational statistics
-
-##Used to devise complez models and algorithms that lend themselves 
+##Used to devise complex models and algorithms that lend themselves 
 ## to a prediction which in commericial use is known as predictive analysis.
 
+#Supervised Learning
+#Unsupervised Learning
+#Reinforcement Learning
 
-##Supervised Learning
-a = 4 + 2 % 5;
-print(a)
+##Classification is the problem of identifying to which set of categories
+##a new observation belongs
 
+import pandas as pd
+from  sklearn.naive_bayes import MultinomialNB
 
-##Unsupervised Learning
+#%%
+
+df = pd.read_csv('golf_df.csv')
+
+print(df.head())
