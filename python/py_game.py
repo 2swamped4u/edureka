e3dbcfafa3039b662c519eb67f0e1d9e563fee55
2swamped4u <jvrjsanchez@gmail.com>
2021-12-16 08:49:15 -0300

    py_game

--- python/py_game.py
+++ python/py_game.py
@@ -5,7 +5,7 @@ screen = pygame.display.set_mode((400,300))
 done = False
 
 while not done:
-	for event in pygame.event.get()
+	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
 
