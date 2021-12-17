629455a642c551ef5abd94fc4b22d1e2291890b5
2swamped4u <jvrjsanchez@gmail.com>
2021-12-17 10:49:33 -0300

    Faster your coding with Loops in Java

---
+++ Loops _Java/src/Demo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Demo {
+
+    private static Scanner sc;
+
+    public static void main(String[] args) {
+        int num, sum = 0;
+        sc = new Scanner(System.in);
+
+        System.out.println("Please enter any integer: ");
+        num = sc.nextInt();
+
+        while (num <= 10) {
+            sum = sum + num;
+            num++;
+        }
+        System.out.println("Sum of the numbers from while loop: " + sum);
+    }
+}
